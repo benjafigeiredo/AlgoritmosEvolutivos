3eea8bb9556451dad030d2776dae7c64b3bd6eb1
bfigeiredo1 <bfigeiredo@tradespark.la>
2022-06-12 17:22:34 -0300

    test de ejecucion TSP

--- TSP/Final/tspFinal.py
+++ TSP/Final/tspFinal.py
@@ -1,10 +1,11 @@
 import tsplib95
-from tsplib95 import *
 
 
 class TSPProblem:
 
-    path = './Resources/Instancias-TSP/br17.atsp'
-
     def __init__(self, path):
         self.problem = tsplib95.load(path)
+
+
+path = './Resources/Instancias-TSP/br17.atsp'
+TSPProblem = TSPProblem(path)
