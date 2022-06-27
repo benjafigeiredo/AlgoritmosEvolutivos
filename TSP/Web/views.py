18305eef3ef412c03cd572b6b3bae5c2759d56f0
bfigeiredo1 <bfigeiredo@tradespark.la>
2022-06-27 11:13:54 -0300

    web primera iteracion TSP

--- TSP/Web/views.py
+++ TSP/Web/views.py
@@ -5,3 +5,11 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route("/TSP")
+def tsp():
+    return render_template("tsp_page.html")
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
